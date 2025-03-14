b739635b12307d7ad4cf35ab18e6a2321214a631
Amiir25 <amirsadik636@gmail.com>
2025-03-14 18:06:38 +0300

    Update render.c

--- render.c
+++ render.c
@@ -1,9 +1,15 @@
 #include "maze.h"
 
-void render(SDL_Renderer *renderer)
-{
-    SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-    SDL_RenderClear(renderer);
+void render(SDL_Renderer *renderer) {
+    // Draw sky (blue)
+    SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
+    SDL_Rect sky = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2};
+    SDL_RenderFillRect(renderer, &sky);
+    
+    // Draw ground (green)
+    SDL_SetRenderDrawColor(renderer, 34, 139, 34, 255);
+    SDL_Rect ground = {0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2};
+    SDL_RenderFillRect(renderer, &ground);
     
     drawWalls(renderer);
     
