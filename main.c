246c0326e1a83e769f0402164e50a56607363baa
Amiir25 <amirsadik636@gmail.com>
2025-03-15 22:03:28 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,20 @@
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Maze Game",
-					  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-					  SCREEN_WIDTH, SCREEN_HEIGHT,
-					  SDL_WINDOW_SHOWN);
-
+    SDL_Window *window = SDL_CreateWindow("Maze Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    
+
+    loadMap();  // Initialize map
     bool running = true;
     SDL_Event event;
+    
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = false;
             }
+            handleInput(event);
         }
-        
         render(renderer);
     }
     
