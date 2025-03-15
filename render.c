caca2d4e386e9b491820fa49a4afcc165bd1ac06
Amiir25 <amirsadik636@gmail.com>
2025-03-15 22:04:48 +0300

    Add render.c

--- render.c
+++ render.c
@@ -5,13 +5,12 @@ void render(SDL_Renderer *renderer) {
     SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
     SDL_Rect sky = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2};
     SDL_RenderFillRect(renderer, &sky);
-    
+
     // Draw ground (green)
     SDL_SetRenderDrawColor(renderer, 34, 139, 34, 255);
     SDL_Rect ground = {0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2};
     SDL_RenderFillRect(renderer, &ground);
-    
+
     drawWalls(renderer);
-    
     SDL_RenderPresent(renderer);
 }
