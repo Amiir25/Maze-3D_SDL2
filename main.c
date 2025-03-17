ab0cc80ddad3027f40e94b50ed889771f7271d91
Amiir25 <amirsadik636@gmail.com>
2025-03-17 09:19:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,44 @@
 #include "maze.h"
 
-int main() {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Maze Game",
-					  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-					  SCREEN_WIDTH, SCREEN_HEIGHT,
-					  SDL_WINDOW_SHOWN);
+/**
+ * main - Entry
+ *
+ * Return: Always zero (Success)
+ */
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+int main(void)
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	SDL_Event event;
+	bool running;
 
-    loadMap();  // Initialize map
-    bool running = true;
-    SDL_Event event;
-    
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-            handleInput(event);
-        }
-        render(renderer);
-    }
-    
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    
-    return 0;
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("Maze Game",
+				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+				SCREEN_WIDTH, SCREEN_HEIGHT,
+				SDL_WINDOW_SHOWN);
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	loadMap();  /* Initialize map */
+	running = true;
+
+	while (running)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				running = false;
+
+			handleInput(event);
+		}
+		render(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return (0);
 }
