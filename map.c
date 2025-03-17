11be71faaf44dba37269fbe3b7e1249fca7bc6f9
Amiir25 <amirsadik636@gmail.com>
2025-03-17 10:22:56 +0300

    Update map.c

--- map.c
+++ map.c
@@ -2,21 +2,28 @@
 
 int map[MAP_HEIGHT][MAP_WIDTH];
 
-void loadMap() {
-    int tempMap[MAP_HEIGHT][MAP_WIDTH] = {
-        {1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 1, 0, 1, 1, 0, 1},
-        {1, 0, 1, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 1, 1, 0, 1},
-        {1, 0, 1, 0, 1, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1}
-    };
+/**
+ * loadMap - Loads the map for the game
+ *
+ * Return: Nothing
+ */
 
-    for (int i = 0; i < MAP_HEIGHT; i++) {
-        for (int j = 0; j < MAP_WIDTH; j++) {
-            map[i][j] = tempMap[i][j];
-        }
-    }
+void loadMap(void)
+{
+	int tempMap[MAP_HEIGHT][MAP_WIDTH] = {
+		{1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 0, 1, 1, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1, 1, 0, 1},
+		{1, 0, 1, 0, 1, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1}
+	};
+
+	for (int i = 0; i < MAP_HEIGHT; i++)
+	{
+		for (int j = 0; j < MAP_WIDTH; j++)
+			map[i][j] = tempMap[i][j];
+	}
 }
