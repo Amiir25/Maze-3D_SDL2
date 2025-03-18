9ec9f645a6598ebabcee75e29e11bc9d1eca541c
Amiir25 <amirsadik636@gmail.com>
2025-03-18 17:54:02 +0300

    Update input.c

--- input.c
+++ input.c
@@ -1,11 +1,21 @@
 #include "maze.h"
 
-void handleInput(SDL_Event event) {
-    if (event.type == SDL_KEYDOWN) {
-        if (event.key.keysym.sym == SDLK_LEFT) {
-            playerAngle -= ROTATION_SPEED;
-        } else if (event.key.keysym.sym == SDLK_RIGHT) {
-            playerAngle += ROTATION_SPEED;
-        }
-    }
+float playerX = 100, playerY = 100;
+float playerAngle = 0.0;
+
+/**
+ * handleInput - Handles keyboard inputs to control player movement.
+ *
+ * @event: The SDL_Event structure containing input data.
+ */
+
+void handleInput(SDL_Event event)
+{
+	if (event.type == SDL_KEYDOWN)
+	{
+		if (event.key.keysym.sym == SDLK_LEFT)
+			playerAngle -= ROTATION_SPEED;
+		else if (event.key.keysym.sym == SDLK_RIGHT)
+			playerAngle += ROTATION_SPEED;
+	}
 }
