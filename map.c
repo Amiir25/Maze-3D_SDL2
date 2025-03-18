fce6c6e82d3c69c2bcf61d4782d05502e155cfdf
Amiir25 <amirsadik636@gmail.com>
2025-03-18 17:53:11 +0300

    Update map.c

--- map.c
+++ map.c
@@ -1,29 +1,24 @@
 #include "maze.h"
 
-int map[MAP_HEIGHT][MAP_WIDTH];
+/* Map: 1 = Wall, 0 = Empty space */
+int map[MAP_HEIGHT][MAP_WIDTH] = {
+	{1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 0, 1, 1, 0, 1},
+	{1, 0, 1, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 1, 1, 0, 1},
+	{1, 0, 1, 0, 1, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1}
+};
 
 /**
- * loadMap - Loads the map for the game
- *
- * Return: Nothing
+ * loadMap - Initializes the game map.
+ * This function is currently a placeholder but allows
+ * easy modification for future dynamic map loading.
  */
 
 void loadMap(void)
 {
-	int tempMap[MAP_HEIGHT][MAP_WIDTH] = {
-		{1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 1, 0, 1, 1, 0, 1},
-		{1, 0, 1, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 1, 1, 0, 1},
-		{1, 0, 1, 0, 1, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1}
-	};
-
-	for (int i = 0; i < MAP_HEIGHT; i++)
-	{
-		for (int j = 0; j < MAP_WIDTH; j++)
-			map[i][j] = tempMap[i][j];
-	}
+	/* In the future, this could load a map from a file */
 }
