3f9cf6dd28b117aa5e951b01caaf6fd258a31349
Amiir25 <amirsadik636@gmail.com>
2025-03-21 16:56:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,51 @@
 #include "maze.h"
 
 /**
- * main - Entry point of the Maze Game.
- * Initializes SDL, creates a window, and runs the game loop.
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Argument values
  *
- * Return: Always 0 (Success).
+ * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 	SDL_Event event;
 	bool running;
 
-	/* Initialize SDL and create window */
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <map_file>\n", argv[0]);
+		return (1);
+	}
+
+	if (!loadMap(argv[1]))
+		return (1);
+
 	SDL_Init(SDL_INIT_VIDEO);
 	window = SDL_CreateWindow("Maze Game",
 				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-				SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
+				SCREEN_WIDTH, SCREEN_HEIGHT,
+				SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	loadMap(); /* Initialize map */
 	running = true;
-
 	while (running)
 	{
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_QUIT)
 				running = false;
+
 			handleInput(event);
 		}
 		render(renderer);
 	}
 
-	/* Cleanup and exit */
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
 	return (0);
 }
