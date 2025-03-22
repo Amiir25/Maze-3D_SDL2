88558112d80520ee2d70a99cbdf3f739f84e22ec
Amiir25 <amirsadik636@gmail.com>
2025-03-22 11:16:00 +0300

    Update walls.c

--- walls.c
+++ walls.c
@@ -1,63 +1,40 @@
 #include "maze.h"
 
+extern SDL_Texture *wallTextures[4];
+
 /**
- * drawWalls - Uses raycasting to draw walls.
+ * drawWalls - Renders the walls with textures.
  * @renderer: The SDL renderer.
  */
-
 void drawWalls(SDL_Renderer *renderer)
 {
-	int x, mapX, mapY, wallHeight, wallTop, wallBottom;
-	bool isVerticalWall;
-	float rayAngle, rayAngleRad, rayX, rayY, deltaX, deltaY, dist;
+    for (int x = 0; x < SCREEN_WIDTH; x++)
+    {
+        float rayAngle = playerAngle + atan((x - SCREEN_WIDTH / 2) / 600.0);
+        int wallHeight = (int)(SCREEN_HEIGHT / castRay(rayAngle));
 
-	for (x = 0; x < NUM_RAYS; x++)
-	{
-		/* Compute the ray angle */
-		rayAngle = (playerAngle - (FOV / 2)) + (x * ((float)FOV / NUM_RAYS));
-		rayAngleRad = rayAngle * M_PI / 180.0; /* Convert to radians */
-		rayX = playerX;
-		rayY = playerY;
-		deltaX = cos(rayAngleRad);
-		deltaY = sin(rayAngleRad);
-		isVerticalWall = false;
+        int wallTop = (SCREEN_HEIGHT / 2) - (wallHeight / 2);
 
-		/* Perform raycasting */
-		while (1)
-		{
-			mapX = (int)(rayX / TILE_SIZE);
-			mapY = (int)(rayY / TILE_SIZE);
+        int textureIndex = getWallTextureIndex(rayAngle);
+        SDL_Texture *wallTexture = wallTextures[textureIndex];
 
-			if (mapX < 0 || mapX >= MAP_WIDTH ||
-			    mapY < 0 || mapY >= MAP_HEIGHT ||
-			    map[mapY][mapX] == 1)
-			{
-				isVerticalWall = fabs(deltaX) > fabs(deltaY);
-				break;
-			}
-			rayX += deltaX;
-			rayY += deltaY;
-		}
+        SDL_Rect destRect = { x, wallTop, 1, wallHeight };
+        SDL_RenderCopy(renderer, wallTexture, NULL, &destRect);
+    }
+}
 
-		/* Correct fish-eye distortion */
-		dist = sqrt(pow(rayX - playerX, 2) + pow(rayY - playerY, 2));
-		dist *= cos((rayAngle - playerAngle) * M_PI / 180.0); /* Adjust for fish-eye */
-		wallHeight = (int)(TILE_SIZE * SCREEN_HEIGHT / (dist + 0.0001)); /* Avoid div by zero */
-
-		/* Compute wall boundaries */
-		wallTop = (SCREEN_HEIGHT / 2) - (wallHeight / 2);
-		wallBottom = (SCREEN_HEIGHT / 2) + (wallHeight / 2);
-		if (wallTop < 0)
-			wallTop = 0;
-		if (wallBottom > SCREEN_HEIGHT)
-			wallBottom = SCREEN_HEIGHT;
-
-		/* Color walls differently based on orientation */
-		if (isVerticalWall)
-			SDL_SetRenderDrawColor(renderer, 80, 80, 80, 255); /* Dark Gray */
-		else
-			SDL_SetRenderDrawColor(renderer, 160, 160, 160, 255); /* Light Gray */
-
-		SDL_RenderDrawLine(renderer, x, wallTop, x, wallBottom);
-	}
+/**
+ * getWallTextureIndex - Determines which wall texture to use.
+ * @angle: The ray angle.
+ * Return: The texture index.
+ */
+int getWallTextureIndex(float angle)
+{
+    if (angle > 45 && angle <= 135)
+        return NORTH;
+    if (angle > 135 && angle <= 225)
+        return WEST;
+    if (angle > 225 && angle <= 315)
+        return SOUTH;
+    return EAST;
 }
