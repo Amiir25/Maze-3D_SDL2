48895686d7eb5786b83b3c045e2ac92e7d6fc341
Amiir25 <amirsadik636@gmail.com>
2025-03-22 17:31:48 +0300

    Update maze.h

--- maze.h
+++ maze.h
@@ -2,65 +2,39 @@
 #define MAZE_H
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <math.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
 #define MAP_WIDTH 10
 #define MAP_HEIGHT 10
-
-#define PLAYER_SPEED 3.0
-#define ROTATION_SPEED 3.0
 #define TILE_SIZE 64
 
-/* Directions for wall textures */
-#define NORTH 0
-#define SOUTH 1
-#define EAST  2
-#define WEST  3
+#define MINIMAP_TILE_SIZE 8
+#define MINIMAP_SCALE 0.125  /* 1/8 scale for minimap */
 
-/* Minimap scaling */
-#define MINIMAP_TILE_SIZE 10
-#define MINIMAP_SCALE 0.2
+#define MOVE_SPEED 5.0
+#define ROTATION_SPEED 3.0
 
-#ifndef M_PI
-    #define M_PI 3.14159265358979323846
-#endif
-
-#define M_PI_2 (M_PI / 2)
-
-/* External variables */
+/* Global Variables */
 extern float playerX, playerY, playerAngle;
 extern int map[MAP_HEIGHT][MAP_WIDTH];
-extern bool isShooting;
+extern bool minimapEnabled;
 
-/* SDL Texture references */
-extern SDL_Texture *wallTextures[4];
-extern SDL_Texture *floorTexture;
-extern SDL_Texture *ceilingTexture;
-extern SDL_Texture *weaponTexture;
-
-/* Function prototypes */
-int loadTextures(SDL_Renderer *renderer);
-void freeTextures(void);
-
+/* Function Declarations */
 void handleInput(SDL_Event event);
-void updatePlayer(void);
-
+void updateMovement(void);
 void render(SDL_Renderer *renderer);
 void drawWalls(SDL_Renderer *renderer);
-void drawFloorAndCeiling(SDL_Renderer *renderer);
-
-float castRay(float angle);
-int getWallTextureIndex(float angle);
-
 void drawMinimap(SDL_Renderer *renderer);
-int loadMap(const char *filename);
-SDL_Texture *loadTexture(SDL_Renderer *renderer, const char *filePath);
+bool loadTextures(SDL_Renderer *renderer);
+void freeTextures(void);
+
+/* Textures */
+extern SDL_Texture *wallTextures[4];
+extern SDL_Texture *ceilingTexture;
+extern SDL_Texture *floorTexture;
 
 #endif /* MAZE_H */
