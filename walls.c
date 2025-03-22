380dfa6a23e1f73ac5c4b50b8c6a0953101ef7c2
Amiir25 <amirsadik636@gmail.com>
2025-03-22 17:31:08 +0300

    Update walls.c

--- walls.c
+++ walls.c
@@ -1,57 +1,54 @@
 #include "maze.h"
 
-extern SDL_Texture *wallTextures[4];
-
 /**
- * drawWalls - Renders the walls with textures.
+ * drawWalls - Uses raycasting to draw textured walls.
  * @renderer: The SDL renderer.
  */
 void drawWalls(SDL_Renderer *renderer)
 {
-    for (int x = 0; x < SCREEN_WIDTH; x++)
+    int x;
+    float rayAngle, rayX, rayY, deltaX, deltaY, dist;
+    int mapX, mapY, wallHeight, textureOffsetX;
+    bool isVerticalWall;
+
+    for (x = 0; x < NUM_RAYS; x++)
     {
-        float rayAngle = playerAngle + atan((x - SCREEN_WIDTH / 2) / 600.0);
-        float distance = castRay(rayAngle);
-        int wallHeight = (int)(SCREEN_HEIGHT / (distance + 0.001)); // Avoid division by zero
+        rayAngle = playerAngle - (FOV / 2) + (x * (float)FOV / NUM_RAYS);
+        rayX = playerX, rayY = playerY;
+        deltaX = cos(rayAngle * M_PI / 180.0);
+        deltaY = sin(rayAngle * M_PI / 180.0);
+        isVerticalWall = false;
 
-        int wallTop = (SCREEN_HEIGHT / 2) - (wallHeight / 2);
-        int textureIndex = getWallTextureIndex(rayAngle);
-        SDL_Texture *wallTexture = wallTextures[textureIndex];
+        while (1)
+        {
+            mapX = (int)(rayX / TILE_SIZE);
+            mapY = (int)(rayY / TILE_SIZE);
 
-        SDL_Rect destRect = { x, wallTop, 1, wallHeight };
-        SDL_RenderCopy(renderer, wallTexture, NULL, &destRect);
+            if (mapX < 0 || mapX >= MAP_WIDTH ||
+                mapY < 0 || mapY >= MAP_HEIGHT ||
+                map[mapY][mapX] == 1)
+            {
+                isVerticalWall = fabs(deltaX) > fabs(deltaY);
+                break;
+            }
+            rayX += deltaX;
+            rayY += deltaY;
+        }
+
+        dist = sqrt(pow(rayX - playerX, 2) + pow(rayY - playerY, 2));
+        wallHeight = (int)(TILE_SIZE * SCREEN_HEIGHT / dist);
+
+        /* Determine wall orientation */
+        int wallSide = isVerticalWall ? (deltaX > 0 ? 2 : 3) : (deltaY > 0 ? 1 : 0);
+
+        /* Texture offset */
+        textureOffsetX = (isVerticalWall ? (int)rayY % TILE_SIZE : (int)rayX % TILE_SIZE);
+
+        /* Wall slice rectangle */
+        SDL_Rect wallSlice = {x, (SCREEN_HEIGHT - wallHeight) / 2, 1, wallHeight};
+        SDL_Rect textureRect = {textureOffsetX, 0, 1, TEXTURE_HEIGHT};
+
+        /* Render the texture slice */
+        SDL_RenderCopy(renderer, wallTextures[wallSide], &textureRect, &wallSlice);
     }
-}
-
-/**
- * getWallTextureIndex - Determines which wall texture to use.
- * @angle: The ray angle.
- * Return: The texture index.
- */
-int getWallTextureIndex(float angle)
-{
-    angle = fmod(angle, 360);
-    if (angle < 0) angle += 360;
-
-    if (angle > 45 && angle <= 135)
-        return NORTH;
-    if (angle > 135 && angle <= 225)
-        return WEST;
-    if (angle > 225 && angle <= 315)
-        return SOUTH;
-    return EAST;
-}
-
-/**
- * castRay - Casts a ray to determine wall distance.
- * @angle: The ray angle.
- * Return: Distance to the nearest wall.
- */
-float castRay(float angle)
-{
-    // Temporary usage to avoid compiler warning
-    (void)angle;
-
-    // Placeholder raycasting logic
-    return 1.0;
 }
