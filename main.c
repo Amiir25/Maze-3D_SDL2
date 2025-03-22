7d4b8bd1e22a6d79d9cf6d3f2bf2916fdb185f01
Amiir25 <amirsadik636@gmail.com>
2025-03-22 11:14:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,45 +7,55 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	SDL_Event event;
-	bool running;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+    bool running;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <map_file>\n", argv[0]);
-		return (1);
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <map_file>\n", argv[0]);
+        return 1;
+    }
 
-	if (!loadMap(argv[1]))
-		return (1);
+    if (!loadMap(argv[1]))
+        return 1;
 
-	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("Maze Game",
-				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-				SCREEN_WIDTH, SCREEN_HEIGHT,
-				SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+    window = SDL_CreateWindow("Maze Game",
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              SCREEN_WIDTH, SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	running = true;
-	while (running)
-	{
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_QUIT)
-				running = false;
+    if (!loadTextures(renderer))
+    {
+        fprintf(stderr, "Failed to load textures!\n");
+        return 1;
+    }
 
-			handleInput(event);
-		}
-		render(renderer);
-	}
+    running = true;
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                running = false;
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	return (0);
+            handleInput(event);
+        }
+	updateMovement();
+        render(renderer);
+    }
+
+    freeTextures();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+
+    return 0;
 }
