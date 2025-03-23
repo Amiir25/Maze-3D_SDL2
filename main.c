1d0ba1cc19c3fdc66c7babf0f9ceac88918b5786
Amiir25 <amirsadik636@gmail.com>
2025-03-23 15:14:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,84 @@
 #include "maze.h"
 
 /**
- * main - Entry point of the Maze Game.
- * Initializes SDL, creates a window, and runs the game loop.
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Argument values
  *
- * Return: Always 0 (Success).
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 	SDL_Event event;
 	bool running;
 
-	/* Initialize SDL and create window */
-	SDL_Init(SDL_INIT_VIDEO);
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <map_file>\n", argv[0]);
+		return (1);
+	}
+
+	/* Load the map */
+	if (!loadMap(argv[1]))
+		return (1);
+
+	/* Initialize SDL */
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		fprintf(stderr, "Error: SDL could not initialize. %s\n", SDL_GetError());
+		return (1);
+	}
+
+	/* Create window */
 	window = SDL_CreateWindow("Maze Game",
 				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-				SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+				SCREEN_WIDTH, SCREEN_HEIGHT,
+				SDL_WINDOW_SHOWN);
+	if (!window)
+	{
+		fprintf(stderr, "Error: Could not create window. %s\n", SDL_GetError());
+		SDL_Quit();
+		return (1);
+	}
 
+	/* Create renderer */
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer)
+	{
+		fprintf(stderr, "Error: Could not create renderer. %s\n", SDL_GetError());
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return (1);
+	}
 
-	loadMap(); /* Initialize map */
+	/* Load textures */
+	if (!loadTextures(renderer))
+	{
+		fprintf(stderr, "Error: Failed to load textures\n");
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return (1);
+	}
+
+	/* Game loop */
 	running = true;
-
 	while (running)
 	{
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_QUIT)
 				running = false;
+
 			handleInput(event);
 		}
 		render(renderer);
 	}
 
-	/* Cleanup and exit */
+	/* Cleanup */
+	freeTextures();
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
