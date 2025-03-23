e31dde7ada8cf9ebd53329eebb6ad173d7edb513
Amiir25 <amirsadik636@gmail.com>
2025-03-23 15:14:16 +0300

    Update maze.h

--- maze.h
+++ maze.h
@@ -1,9 +1,15 @@
 #ifndef MAZE_H
 #define MAZE_H
 
+#include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdbool.h>
 #include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -14,16 +20,20 @@
 #define NUM_RAYS SCREEN_WIDTH
 #define ROTATION_SPEED 5.0
 
-#ifndef M_PI
-    #define M_PI 3.14159265358979323846
-#endif
-
 /* Global variables */
 extern float playerX, playerY, playerAngle;
+extern float moveSpeed;
 extern int map[MAP_HEIGHT][MAP_WIDTH];
 
+/* SDL Texture variables */
+extern SDL_Texture *wallTexture;
+extern SDL_Texture *floorTexture;
+extern SDL_Texture *ceilingTexture;
+
 /* Function prototypes */
-void loadMap(void);
+int loadMap(const char *filename);
+int loadTextures(SDL_Renderer *renderer);
+void freeTextures();
 void handleInput(SDL_Event event);
 void render(SDL_Renderer *renderer);
 void drawWalls(SDL_Renderer *renderer);
