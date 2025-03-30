09ee612af417d93d1dae4ea83c56867d03fbb32e
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-30 15:27:42 +0300

    Update render.c

--- render.c
+++ render.c
@@ -4,23 +4,28 @@
  * drawFloorAndCeiling - Draws the textured floor and ceiling.
  * @renderer: The SDL renderer.
  */
+
 void drawFloorAndCeiling(SDL_Renderer *renderer)
 {
     SDL_Rect floorRect = { 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 };
     SDL_Rect ceilingRect = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2 };
 
+    /* Check if a floor texture is available */
     if (floor_texture)
         SDL_RenderCopy(renderer, floor_texture, NULL, &floorRect);
     else
     {
+        /* Draw a solid color floor if no texture is available */
         SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
         SDL_RenderFillRect(renderer, &floorRect);
     }
 
+    /* Check if a ceiling texture is available */
     if (ceiling_texture)
         SDL_RenderCopy(renderer, ceiling_texture, NULL, &ceilingRect);
     else
     {
+        /* Draw a solid color ceiling if no texture is available */
         SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
         SDL_RenderFillRect(renderer, &ceilingRect);
     }
