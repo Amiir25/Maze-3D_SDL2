d3efc85c99766d88c8b9a19fac2eaa9f4dc6a31b
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-03-30 15:26:12 +0300

    Update maze.h

--- maze.h
+++ maze.h
@@ -1,16 +1,19 @@
 #ifndef MAZE_H
 #define MAZE_H
 
+/* Header files */
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
 #include <math.h>
 
+/* Value of PI */
 #ifndef M_PI
-#define M_PI 3.14159265358979323846
+    #define M_PI 3.14159265358979323846
 #endif
 
+/* Preprocessor directives */
 #define SCREEN_WIDTH 1400
 #define SCREEN_HEIGHT 700
 #define MAP_WIDTH 8
@@ -20,6 +23,12 @@
 #define NUM_RAYS SCREEN_WIDTH
 #define ROTATION_SPEED 5.0
 
+/* Global variables */
+extern float playerX, playerY, playerAngle;
+extern float moveSpeed;
+extern int map[MAP_HEIGHT][MAP_WIDTH];
+
+/* SDL Texture variables */
 extern SDL_Texture *wall_north;
 extern SDL_Texture *wall_south;
 extern SDL_Texture *wall_east;
@@ -27,12 +36,6 @@ extern SDL_Texture *wall_west;
 extern SDL_Texture *floor_texture;
 extern SDL_Texture *ceiling_texture;
 
-/* Global variables */
-extern float playerX, playerY, playerAngle;
-extern float moveSpeed;
-extern int map[MAP_HEIGHT][MAP_WIDTH];
-
-/* SDL Texture variables */
 extern SDL_Texture *wallTexture;
 extern SDL_Texture *floorTexture;
 extern SDL_Texture *ceilingTexture;
